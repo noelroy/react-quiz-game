c0d807a06a7ff67c6e6fd20c276ca5a139e62e29
noelroy <19671254+noelroy@users.noreply.github.com>
2022-01-15 01:23:50 +0530

    Deply to github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import ResultPage from './pages/ResultPage';
 import GamePage from './pages/GamePage';
@@ -7,14 +7,14 @@ import GlobalStyle from './App.style';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle />
        <Routes>
          <Route path="/" element={<StartPage />} />
          <Route path="result" element={<ResultPage />} />
          <Route path="game" element={<GamePage />} />
        </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
